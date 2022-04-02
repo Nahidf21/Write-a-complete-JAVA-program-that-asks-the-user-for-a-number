dafad8926859d2630d8af0c63d932c5531c209cc
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2022-04-01 22:01:02 -0500

    main file

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner scr = new Scanner(System.in);
+		Scanner scr1 = new Scanner(System.in);
+		
+		int number1=0;
+		
+		while(number1 < 10) {
+			
+			System.out.println("Enter the number: ");
+			int number= scr.nextInt();
+			if (number >= -25 && number <=25) {
+				System.out.println("This numbe is in the range");
+			}
+			else if(number < -25 || number > 25) {
+				System.out.println("This numbe is out of the range");
+			}
+			
+			System.out.println("If you want to continue the program the Type: y or not then type n : ");
+			char disision = scr1.nextLine().charAt(0);
+			if (disision=='n') {
+				number1=10;
+				System.out.println("Work done");
+			}
+		}
+			
+	}
+
+}
